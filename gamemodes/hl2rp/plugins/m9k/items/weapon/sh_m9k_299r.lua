c642fa154396399e7eaae4dd6b16baefb051ef37
NDDev <NDDev@users.noreply.github.com>
2014-12-11 14:31:14 -0500

    Update sh_m9k_299r.lua
    testing weapon buy-flags with ONE weapon
    likely resolves #5

--- gamemodes/hl2rp/plugins/m9k/items/weapon/sh_m9k_299r.lua
+++ gamemodes/hl2rp/plugins/m9k/items/weapon/sh_m9k_299r.lua
@@ -3,4 +3,5 @@ ITEM.desc = "A full-sized, service-type pistol by SIG Sauer."
 ITEM.model = Model("models/weapons/w_sig_229r.mdl")
 ITEM.class = string.gsub(ITEM.uniqueID, "weapon_", "")
 ITEM.type = "pistol"
-ITEM.price = 1000
+ITEM.flag = "w"
+ITEM.price = 800
