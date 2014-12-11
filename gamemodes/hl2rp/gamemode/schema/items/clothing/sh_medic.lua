2335ebcf2fb7be7428651633b294c1044dde2950
NDDev <NDDev@users.noreply.github.com>
2014-12-11 14:21:30 -0500

    Update sh_medic.lua
    closes #19

--- gamemodes/hl2rp/gamemode/schema/items/clothing/sh_medic.lua
+++ gamemodes/hl2rp/gamemode/schema/items/clothing/sh_medic.lua
@@ -2,6 +2,6 @@ ITEM.name = "Rebel Medic Outfit"
 ITEM.desc = "A medical variant of the rebel outfit."
 ITEM.model = Model("models/humans/group03m/male_04.mdl")
 ITEM.replacement = {"group(%d+)", "group03m"}
-ITEM.flag = "v"
+ITEM.flag = "r"
 ITEM.price = 150
 ITEM.weight = -3
